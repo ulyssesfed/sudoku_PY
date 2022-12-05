54c0c1837dfdd828092f8dbc27afc4c2e9fe17ec
uly <ulyfleen@gmail.com>
2022-12-05 21:12:48 +0000

    added the ability to change difficulty and a simple sort of menu system

--- main.py
+++ main.py
@@ -26,9 +26,14 @@ def can_place(grid, row, col, num):
     return True
 
 # Generate an unsolved Sudoku puzzle by making a random number of random moves
-def generate_unsolved_sudoku():
-    # Make a random number of random moves (50-70 moves should be enough)
+def generate_unsolved_sudoku(difficulty):
+    # Make a random number of random moves
     num_moves = random.randint(50, 70)
+    if difficulty == "easy":
+        num_moves -= 10
+    elif difficulty == "hard":
+        num_moves += 10
+
     for _ in range(num_moves):
         row = random.randint(0, 8)
         col = random.randint(0, 8)
@@ -40,5 +45,19 @@ def generate_unsolved_sudoku():
     for row in grid:
         print(row)
 
-# Generate and print an unsolved Sudoku puzzle
-generate_unsolved_sudoku()
+# Main program loop
+while True:
+    # Generate and print an unsolved Sudoku puzzle of the given difficulty level
+    difficulty = input("Enter the desired difficulty level (easy, medium, or hard) or 'exit' to quit: ")
+    if difficulty == "exit":
+        break
+    generate_unsolved_sudoku(difficulty)
+
+    # Ask the user if they want to try another puzzle or change the difficulty level
+    choice = input("Enter 'again' to try another puzzle or 'change' to change the difficulty level: ")
+    if choice == "change":
+        continue
+    elif choice == "again":
+        continue
+    else:
+        break
